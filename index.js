961f3f6aa6cc21f23556138c42a1f3e50d2980b1
Bhavya Verma <vbhavya269@gmail.com>
2022-10-23 00:46:36 +0530

    ⚡️ Added middlewares

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// dotenv config
 dotenv.config();
 
+// connect to mongodb
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('Connected to MongoDB');
 });
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
 
 app.listen(8800, () => {
     console.log('Backend server is running on port 8800!');
