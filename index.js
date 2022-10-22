e7af2f3e4b9632173d1d8b2c1db5406c01067abc
Bhavya Verma <vbhavya269@gmail.com>
2022-10-23 00:37:39 +0530

    ✨ Created MongoDB connection

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const morgan = require('morgan');
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('Connected to MongoDB');
+});
+
 app.listen(8800, () => {
     console.log('Backend server is running on port 8800!');
 })
