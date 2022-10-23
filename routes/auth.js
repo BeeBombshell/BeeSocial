a918d515bb7f8b6f677ba70e4994f6ce0fb4a438
Bhavya Verma <vbhavya269@gmail.com>
2022-10-23 16:07:21 +0530

    ✨ Added Users DB Schema

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get("/", (req, res) => {
-    res.send("Hey! Its auth route!");
+// register
+router.get("/register", async (req, res) => {
+    const user = await new User({
+        username: "johnny",
+        email: "john@gmail.com",
+        password: "123456"
+    })
+
+    await user.save();
+    res.send("ok");
 })
 
 module.exports = router;
