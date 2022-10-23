9935aa80a4c8ba40535608fd216b263fa17f74e1
Bhavya Verma <vbhavya269@gmail.com>
2022-10-23 19:44:20 +0530

    ⚡️ Added registering through requests

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,19 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 // register
-router.get("/register", async (req, res) => {
-    const user = await new User({
-        username: "johnny",
-        email: "john@gmail.com",
-        password: "123456"
-    })
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
 
-    await user.save();
-    res.send("ok");
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 module.exports = router;
