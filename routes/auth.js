0079e060038947aea76a8250e11f4a29c0a1e33c
Bhavya Verma <vbhavya269@gmail.com>
2022-10-26 23:37:48 +0530

    ⚡️ Hashing the password

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 // register
 router.post("/register", async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    });
-
     try {
+        // generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        // save user and return response
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (err) {
