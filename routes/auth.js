e35a43fe5bd22af83d8e598d32f6399a583098f6
Bhavya Verma <vbhavya269@gmail.com>
2022-10-27 20:08:19 +0530

    ⚡️ Login route (tests wrong password)

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,10 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     try{
         const user = await User.findOne({ email: req.body.email });
-        !user && res.status(404).json("user not found")
+        !user && res.status(404).json("user not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("wrong password")
     }
     catch (err) {
         console.log(err);
