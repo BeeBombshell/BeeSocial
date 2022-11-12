4fdb2aff609270f93a4c1b88d7204f4858551a7a
BeeBombshell <vbhavya269@gmail.com>
2022-11-12 14:14:15 +0530

    ✨ Added update user route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,31 @@
 const User = require('../models/User')
 const router = require('express').Router();
+const bcrypt = require('bcrypt')
 
 // update user
+router.put("/:id", async(req, res)=> {
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }
+            catch(err) {
+                return res.status(500).json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {$set: req.body,});
+            res.status(200).json("Account has been updated!")
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    }
+    else {
+        return res.status(403).json("You can update only your account!")
+    }
+})
 
 // delete user
 
