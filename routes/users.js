736cab570cef423bc6610788e0ba06595714dbe9
BeeBombshell <vbhavya269@gmail.com>
2022-11-14 14:38:47 +0530

    ✨ Added delete user route

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt')
 
 // update user
 router.put("/:id", async(req, res)=> {
-    if(req.body.userId === req.params.id || req.user.isAdmin){
+    if(req.body.userId === req.params.id || req.body.isAdmin){
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
@@ -28,6 +28,20 @@ router.put("/:id", async(req, res)=> {
 })
 
 // delete user
+router.delete("/:id", async(req, res)=> {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted successfully!")
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    }
+    else {
+        return res.status(403).json("You can delete only your account!")
+    }
+})
 
 // get a user
 
