961b1c8eeba3fe5eceaac14a9615d5722c683d11
BeeBombshell <vbhavya269@gmail.com>
2022-11-24 17:38:21 +0530

    ⚡️ Removed unwanted params from get a user

--- routes/users.js
+++ routes/users.js
@@ -44,10 +44,11 @@ router.delete("/:id", async(req, res)=> {
 })
 
 // get a user
-router.get("/:id", async(req, res)=> {
+router.get("/:id", async( req, res) => {
     try {
-        const user = await User.findById(req.params. id);
-        res.status(200).json(user)
+        const user = await User.findById(req.params.id);
+        const  {password, updatedAt, ...other} = user._doc;
+        res.status(200).json(other);
     }
     catch(err) {
         res.status(500).json(err);
