6bbe57f8e984a8bab7c9241dc7cf842906e4eb49
BeeBombshell <vbhavya269@gmail.com>
2022-12-11 23:20:33 +0530

    ✨ Added unfollow user route

--- routes/users.js
+++ routes/users.js
@@ -80,5 +80,28 @@ router.put("/:id/follow", async (req, res) => {
 })
 
 // unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({$pull: {followers: req.body.userId}});
+                await currentUser.updateOne({$pull: {followings: req.params.id}});
+                res.status(200).json("User has been unfollowed!");
+            }
+            else{
+                res.status(403).json("You do not follow this user!");
+            }
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    }
+    else {
+        res.status(403).json("You can't follow yourself!")
+    }
+})
+
 
 module.exports = router;
