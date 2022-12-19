28463619a6eb5669c1008fbb603b38f5a60c1837
BeeBombshell <vbhavya269@gmail.com>
2022-12-19 21:50:35 +0530

    🔨 Added Post Route in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
+const PostRoute = require('./routes/posts');
 
 // dotenv config
 dotenv.config();
@@ -24,6 +25,7 @@ app.use(morgan('common'));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", PostRoute);
 
 
 app.listen(8800, () => {
---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+    console.log("post page");
+})
+
+module.exports = router;
