eef0c9d8ca1d2971d63224d510fe8e41d1488021
BeeBombshell <vbhavya269@gmail.com>
2022-12-28 00:35:14 +0530

    ✨ Added route for updating a post

--- routes/posts.js
+++ routes/posts.js
@@ -11,9 +11,24 @@ router.post("/", async (req, res) => {
     catch(err) {
         res.status(500).json(err);
     }
-})
+});
 
 // update a post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.updateOne({$set: req.body});
+            res.status(200).json("The post has been updated!");
+        }
+        else {
+            res.status(404).json("Post not found!")
+        }
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 // delete a post
 
