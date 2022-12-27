2f5e68e2fedf62f4442a0f8da640a18fdb0192f5
BeeBombshell <vbhavya269@gmail.com>
2022-12-28 00:06:32 +0530

    ✨ Added route for create post

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,28 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-    console.log("post page");
+// create a post
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
 })
+
+// update a post
+
+// delete a post
+
+// like a post
+
+// get a post
+
+// get timeline posts (all posts of followed users)
+
+
 
 module.exports = router;
