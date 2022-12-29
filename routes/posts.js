ffaedb45b42adf3e1eaa3d885e4c36c020fa352a
BeeBombshell <vbhavya269@gmail.com>
2022-12-30 02:00:30 +0530

    ✨ Added like/dislike route

--- routes/posts.js
+++ routes/posts.js
@@ -47,7 +47,23 @@ router.delete("/:id", async (req, res) => {
     }
 })
 
-// like a post
+// like/dislike a post
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push: {likes: req.body.usserId}});
+            res.status(200).json("The post has been liked!");
+        }
+        else {
+            await post.updateOne({$pull: {likes: req.body.userId}});
+            res.status(200).json("The post has been disliked!");
+        }
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 // get a post
 
