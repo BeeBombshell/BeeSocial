d148e8b9533d16ed9a42d958ef6a771082a158bd
BeeBombshell <vbhavya269@gmail.com>
2022-12-31 00:28:53 +0530

    ✨ Added get a post route

--- routes/posts.js
+++ routes/posts.js
@@ -66,6 +66,15 @@ router.put("/:id/like", async (req, res) => {
 })
 
 // get a post
+router.get("/:id", async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 // get timeline posts (all posts of followed users)
 
